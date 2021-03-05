79bb35257a83129b9b5f864445f227cc52f797ef
James Wyn <thisisjameswyn@gmail.com>
2021-03-05 09:18:25 -0800

    add all setup files and basic funtionality

---
+++ BakeryOrder/Controllers/VendorController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using BakeryOrder.Models;
+using System.Collections.Generic;
+
+namespace BakeryOrder.Controllers
+{
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendor")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendor/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendor")]
+    public ActionResult Create(string cityName)
+    {
+      Vendor myVendor = new Vendor(cityName);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/vendor/delete")]
+    public ActionResult DeleteAll()
+    {
+      Vendor.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/vendor/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor foundVendor = Vendor.Find(id);
+      return View(foundVendor);
+    }
+  }
+}
