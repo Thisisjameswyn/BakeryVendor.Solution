7773b7831b37325a0a979089a5dcbb255c293846
James Wyn <thisisjameswyn@gmail.com>
2021-03-05 16:51:19 -0800

    add logic for order

--- BakeryOrder/Controllers/VendorController.cs
+++ BakeryOrder/Controllers/VendorController.cs
@@ -40,5 +40,18 @@ namespace BakeryOrder.Controllers
       Vendor foundVendor = Vendor.Find(id);
       return View(foundVendor);
     }
+
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category foundCategory = Category.Find(categoryId);
+      Item newItem = new Item(itemDescription);
+      foundCategory.AddItem(newItem);
+      List<Item> categoryItems = foundCategory.Items;
+      model.Add("items", categoryItems);
+      model.Add("category", foundCategory);
+      return View("Show", model);
+    }
   }
 }
