4b7e4acc879e038263591c7be9ef8bd77a788c4b
James Wyn <thisisjameswyn@gmail.com>
2021-05-17 10:17:08 -0700

    add project push for submit

--- BakeryOrder.Tests/ModelTests/OrderTests.cs
+++ BakeryOrder.Tests/ModelTests/OrderTests.cs
@@ -5,13 +5,13 @@ using BakeryOrder.Models;
 
 namespace BakeryOrder.Tests
 {
-  [TestClass]
-  public class OrderTests
-  {
-    [TestMethod]
-    public void CostPush_ReturnOrderAmount_OrderCalc()
-    {
+  // [TestClass]
+  // public class OrderTests
+  // {
+  //   [TestMethod]
+  //   public void CostPush_ReturnOrderAmount_OrderCalc()
+  //   {
 
-    }
-  }
+  //   }
+  // }
 }
